599236b92c8c1bfbf4573fd72413ed35b2563186
Olena Dudnyk <Olena_Dudnyk@epam.com>
2019-07-16 14:54:32 +0300

    Fix timer

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,7 @@
 import '../scss/index.scss';
 import startGame from "./startGame";
 import finishGame from "./finishGame";
-
+import Timer from './timer';
 const timer = new Timer();
 timer.start();
 
